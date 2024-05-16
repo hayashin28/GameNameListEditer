a318145cae8422ea809bfbaacff65b55d0ce868c
hayashin28 <hayashin.28@gmail.com>
2024-05-16 13:36:38 +0900

    プロト版0.01リリース

---
+++ MainGui.py
@@ -0,0 +1,90 @@
+from Dialog.XMLSelecterDialog import XMLSelecterDialog
+from Dialog.LPLSelecterDialog import LPLSelecterDialog
+from Parser.XMLParser import XMLParser
+import PySimpleGUI as sg
+
+
+#テーマカラーをsg.themeで設定
+sg.theme('NeutralBlue') # 'Dark Brown'
+
+#sg.Frameでフレームを定義
+#フレーム1(中はからっぽ、フレームサイズだけ指定)
+frame1 = sg.Frame('',
+    [] , size=(500, 700) #幅,高さ
+)
+
+#フレーム2(中はからっぽ、フレームサイズだけ指定)
+frame2 = sg.Frame('',
+    [] , size=(500, 700) #幅,高さ
+)
+
+#全体レイアウト
+"""
+レイアウトの中に記述する[]が「1行」を表している
+今回はframe1と2を横に並べるので、同じ[]の中に記述する
+"""
+frame1 = [
+    #以下[]で1行の扱いになる。カンマ区切りで横に部品を並べられる
+    [
+        sg.Text('XMLファイルを選択', key='XMLTEXT')
+    ],
+    [
+        sg.Input(key='XMLPATH', readonly=True), sg.Button('ボタン', key='XMLBTN')
+    ],
+]
+
+frame2 = [
+    #以下[]で1行の扱いになる。カンマ区切りで横に部品を並べられる
+    [
+        sg.Text('LPLファイルを選択', key='LPLTEXT')
+    ],
+    [
+        sg.Input(key='LPLPATH', readonly=True), sg.Button('ボタン', key='LPLBTN')
+    ],
+]
+
+layout = [
+    [
+        sg.Frame('',frame1),
+    ],
+    [
+        sg.Frame('',frame2),
+    ],
+    [
+        sg.Button('置換実行', disabled=False, key='REPBTN'),
+    ],
+]
+
+#GUIタイトルと全体レイアウトをのせたWindowを定義する。画面サイズは省略OK
+#resizableでWindowサイズをマウスで変更できるようになる
+window = sg.Window('', layout, resizable=False)
+window.finalize()
+
+#GUI表示実行部分
+while True:
+    # ウィンドウ表示
+    event, values = window.read()
+
+    #クローズボタンの処理
+    if event in (sg.WIN_CLOSED, event is None):
+        break
+    
+    elif event == 'XMLBTN':
+        instance = XMLSelecterDialog()
+        path = instance.XMLSelecterDialog()        
+        window['XMLPATH'].Update(path)  # テキスト追記 # type: ignore 
+                   
+    elif event == 'LPLBTN':
+        instance = LPLSelecterDialog()
+        path = instance.LPLSelecterDialog()
+        window['LPLPATH'].Update(path)  # テキスト追記 # type: ignore 
+
+    elif event == 'REPBTN':
+        xmlPath = values['XMLPATH']
+        lplPath = values['LPLPATH']
+        instance = XMLParser()
+        instance.XMLParser(xmlPath, lplPath)
+   
+    window.refresh()                    # 画面更新
+
+window.close()
--- Parser/XMLParser.py
+++ Parser/XMLParser.py
@@ -1,6 +1,39 @@
+import shutil
 import xml.etree.ElementTree as ET
 
 class XMLParser:
     
-    def XMLParser(self):
-        test:str = "";
+        def __init__(self):   # インスタンス生成時に自動的に呼ばれるメソッド
+            pass
+    
+        # xmlの解析
+        def XMLParser(self, xmlPath:str, lplPath:str):
+        
+            shutil.copy(lplPath, lplPath+'.bk')
+            # 既存ファイルの読み込み
+            with open(lplPath, encoding='UTF-8') as reader:    
+                data_lines = reader.read()
+        
+        
+            tree = ET.parse(xmlPath)
+            root = tree.getroot()
+
+            root.tag
+            root.attrib
+
+            for child in root:
+                atrb:str = child.get('name') # type: ignore
+                for gran in child:
+                    altName:str = gran.get('name_alt') # type: ignore
+                    
+                    if altName != None:
+                        # 指定したファイルの文字列を置換する
+                        data_lines = data_lines.replace(atrb, altName)
+
+            # 3.同じファイル名で保存
+            with open(lplPath, mode='w', encoding='UTF-8') as writer:    
+                writer.write(data_lines)
+                    
+                    
+                    
+                    
